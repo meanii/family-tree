8423bc2521cb9fdbcdce903ea3ab7ddffe8b5520
anil <anilchauhanxda@gmail.com>
2024-03-26 23:09:47 +0530

    Added new command 'list': list of relationships and people in table format

---
+++ cmd/list.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/meanii/family-tree/database"
+	"github.com/rodaine/table"
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list all relationships or persons",
+	Long: `list all relationships or persons. For example:
+family-tree list --relationship
+family-tree list --person`,
+	Run: func(cmd *cobra.Command, args []string) {
+		relationship, _ := cmd.Flags().GetBool("relationship")
+		person, _ := cmd.Flags().GetBool("person")
+
+		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		columnFmt := color.New(color.FgYellow).SprintfFunc()
+		if relationship {
+			tbl := table.New("ID", "Type")
+			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+			for _, relationship := range database.Database.GetRelationships() {
+				tbl.AddRow(relationship.ID, relationship.Type)
+			}
+			tbl.Print()
+		} else if person {
+			tbl := table.New("ID", "Name", "Gender", "Family Root")
+			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+			for _, person := range database.Database.GetPeople() {
+				tbl.AddRow(person.ID, person.Name, person.Gender, person.FamilyRoot)
+			}
+			tbl.Print()
+		} else {
+			fmt.Println("Please specify either --relationship or --person")
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolP("relationship", "r", false, "List all relationships")
+	listCmd.Flags().BoolP("person", "p", false, "List all persons")
+}
